0374e9e96f949d45efc8685790867e8dfb3bf7c3
rich@wight <r.j.henry@gmail.com>
2023-12-04 11:30:31 +0000

    Added a few files, no compiler as yet

---
+++ wut/Plus.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void plusFunc(std::string str) {
+  std::cout << "Plus string is " << str << std::endl;
+}
---
+++ wut/Trad.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void tradFunc(char *str) {
+  printf("The trad string is %s\n");
+}
