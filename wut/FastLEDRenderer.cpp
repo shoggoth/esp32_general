6f3accaba6dc4899da567be0b562483769d56254
rich@wight <r.j.henry@gmail.com>
2023-12-06 11:16:56 +0000

    Have FastLED on thread but spazzing out with other LED on

--- wut/FastLEDRenderer.cpp
+++ wut/FastLEDRenderer.cpp
@@ -4,12 +4,13 @@
 FastLEDRenderer::FastLEDRenderer(const uint8_t v, const uint32_t ma) {
   std::cout << "Constructin" << std::endl;
   FastLED.setMaxPowerInVoltsAndMilliamps(v, ma);
-  FastLED.addLeds<NEOPIXEL, led_pin>(leds, num_leds).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<NEOPIXEL, led_pin>(leds, num_leds).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<NEOPIXEL, led_pin>(leds, num_leds);
   FastLED.clearData();
 }
 
 void FastLEDRenderer::render() {
   std::cout << "Rendering" << std::endl;
-  leds[0] = CRGB(0, 0, 255);
+  leds[0] = CRGB(rand() % 255, 0, 0);
   FastLED.show();
 }
--- wut/wut.ino
+++ wut/wut.ino
@@ -12,6 +12,7 @@ static auto tlam = [](std::string s) {
   };
 static std::unique_ptr<std::thread> thread_ptr_1;
 static std::unique_ptr<std::thread> thread_ptr_2;
+static std::unique_ptr<std::thread> thread_ptr_3;
 
 class Foo {
 };
@@ -21,13 +22,20 @@ const short led_pin = 10;  // Status LED (blue)
 
 void setup() {
   // put your setup code here, to run once:
-  delay(1000);
+  //delay(1000);
 
   thread_ptr_1 = std::unique_ptr<std::thread>(new std::thread(tlam, "One"));
   thread_ptr_2 = std::unique_ptr<std::thread>(new std::thread(tlam, "Two"));
-  threadMessing();
+  thread_ptr_3 = std::unique_ptr<std::thread>(new std::thread([] { for (;;) { renderer->render(); delay(100); }}));
   
-  [](std::string s) { std::cout << "Anon lambda " << s << std::endl; } ("Hoo boy!");
+  threadMessing();
+  thread_ptr_1->detach();
+  thread_ptr_2->detach();
+  thread_ptr_3->detach();
+  
+  //[](std::string s) { std::cout << "Anon lambda " << s << std::endl; } ("Hoo boy!");
+
+  //renderer->render();
 
   pinMode(led_pin, OUTPUT);
 }
@@ -36,12 +44,12 @@ static int pixel = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  std::cout << "Going round the main loop: " << std::endl;
+  //std::cout << "Going round the main loop: " << std::endl;
 
-  renderer->render();
-  digitalWrite(led_pin, HIGH);
+  //renderer->render();
+  //digitalWrite(led_pin, HIGH);
   delay(100);
 
-  digitalWrite(led_pin, LOW);
+  //digitalWrite(led_pin, LOW);
   delay(100);
 }
